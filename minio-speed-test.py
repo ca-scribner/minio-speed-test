0db5ae90040f600525c5ab9a222f6654809286b7
Andrew Scribner <ca.scribner+1@gmail.com>
2020-09-30 16:03:16 +0000

    Add gitignore, modify reporting

--- minio-speed-test.py
+++ minio-speed-test.py
@@ -1,4 +1,6 @@
+import os
 import pandas as pd
+import numpy as np
 import collections
 import shutil
 import subprocess
@@ -13,11 +15,18 @@ def time_copy(src, dst, n=1):
     """
     Copies src to dst n times, returning a list of the copy times in seconds
     """
+    print(f"Copying {src} --> {dst}")
     times = []
+    # NOTE: shutil.copy() does not rais an error if the copy fails on minio
+    # drive, at least not if it fails because dst does not exist.  Test around
+    # this.
     for i in range(n):
         timer = Timer()
         shutil.copy(src, dst)
         times.append(timer.elapsed())
+    if not os.path.exists(dst):
+        raise ValueError("Destination does not exist after copy - likely"
+                         " copied to a non-existant minio directory")
     return times
 
 
@@ -35,26 +44,33 @@ def create_files(filename_prefix="tempfile", size="1m", n=1):
 
 def run_case(filenames, size, n, src, dst):
     result = {'size': size, 'n': n, 'src': src, 'dst': dst}
-    result['times'] = []
+    times = []
     for filename in filenames:
-        result['times'].append(time_copy(f"{src}/{filename}", dst)[0])
-    result['avg_time'] = sum(result['times']) / len(result['times'])
+        times.append(time_copy(f"{src}/{filename}", dst)[0])
+    size_in_bytes = size_to_bytes(size)
+    result['avg_time'] = sum(times) / len(times)
+    result['avg_mb_per_s'] = size_in_bytes / result['avg_time'] / 1000000.
+    result['std'] = np.std(times)
+    result['min'] = min(times)
+    result['max'] = max(times)
+    result['times'] = times
+    result['size_in_bytes'] = size_in_bytes
     return result
 
 
 def main():
-    output_file = "timing_output.txt"
+    output_file = "timing_output.csv"
     cases = [
-        {'size': '1k', 'n': 100, 'tenant': 'minimal-tenant1'},
+        {'size': '1k', 'n': 1000, 'tenant': 'minimal-tenant1'},
         {'size': '1m', 'n': 100, 'tenant': 'minimal-tenant1'},
-        {'size': '10m', 'n': 10, 'tenant': 'minimal-tenant1'},
-        {'size': '100m', 'n': 10, 'tenant': 'minimal-tenant1'},
-        {'size': '1g', 'n': 5, 'tenant': 'minimal-tenant1'},
-        {'size': '1k', 'n': 100, 'tenant': 'premium-tenant1'},
-        {'size': '1m', 'n': 100, 'tenant': 'premium-tenant1'},
-        {'size': '10m', 'n': 10, 'tenant': 'premium-tenant1'},
-        {'size': '100m', 'n': 10, 'tenant': 'premium-tenant1'},
-        {'size': '1g', 'n': 5, 'tenant': 'premium-tenant1'},
+#         {'size': '10m', 'n': 10, 'tenant': 'minimal-tenant1'},
+#         {'size': '100m', 'n': 10, 'tenant': 'minimal-tenant1'},
+#         {'size': '1g', 'n': 5, 'tenant': 'minimal-tenant1'},
+#         {'size': '1k', 'n': 100, 'tenant': 'premium-tenant1'},
+#         {'size': '1m', 'n': 100, 'tenant': 'premium-tenant1'},
+#         {'size': '10m', 'n': 10, 'tenant': 'premium-tenant1'},
+#         {'size': '100m', 'n': 10, 'tenant': 'premium-tenant1'},
+#         {'size': '1g', 'n': 5, 'tenant': 'premium-tenant1'},
     ]
 
     results = []
@@ -67,8 +83,8 @@ def main():
 
         local_source_dir = "./source/"
         local_target_dir = "./target/"
-        minio_target_dir1 = f"~/minio/{tenant}/private/minio_speed_test/target1/"
-        minio_target_dir2 = f"~/minio/{tenant}/private/minio_speed_test/target2/"
+        minio_target_dir1 = f"/home/jovyan/minio/{tenant}/private/minio_speed_test/target1/"
+        minio_target_dir2 = f"/home/jovyan/minio/{tenant}/private/minio_speed_test/target2/"
         # minio_target_dir1 = f"./target1/"
         # minio_target_dir2 = f"./target2/"
         all_directories = [local_source_dir, local_target_dir, minio_target_dir1, minio_target_dir2]
@@ -91,6 +107,8 @@ def main():
         # Copy from minio to minio
         results.append(run_case(filenames, size, n, minio_target_dir1, minio_target_dir2))
 
+#         input("press something to continue")
+
         # Cleanup files
         for d in all_directories:
             for f in filenames:
@@ -99,11 +117,6 @@ def main():
         # Write current results (so we can easily monitor progress - a little wasteful but whatever...)
         current_results = pd.DataFrame(results)
         current_results.to_csv(output_file)
-
-
-if __name__ == "__main__":
-    main()
-
 
 # Helpers
 def is_iterable(arg):
@@ -140,3 +153,19 @@ def make_iterable(arg):
         return arg
     else:
         return [arg]
+
+
+def size_to_bytes(size):
+    suffixes = {
+        'k': '000',
+        'm': '000000',
+        'g': '000000000',
+        't': '000000000000',
+    }
+    for suffix, zeros in suffixes.items():
+        size = size.replace(suffix, zeros)
+    return int(size)
+
+
+if __name__ == "__main__":
+    main()
